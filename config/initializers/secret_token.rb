474031909cb55cdcd2370ccffc3889ba72e6f248
yakkuru201 <tonamimagic@gmail.com>
2013-12-11 17:48:53 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HogeKeijiban::Application.config.secret_token = 'ce7c030fad8bd8a55c709fd73d61d51c65361b645835fadbe09d529422614fa831166749f110cb69c8708359f26f667f0ef865771a8f88f3842e2bff23497973'
