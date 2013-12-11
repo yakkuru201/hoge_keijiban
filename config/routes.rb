08f765b4f7e86e6581911f520fe35ddf8c7acfa8
yakkuru201 <tonamimagic@gmail.com>
2013-12-11 18:03:00 +0900

    define resource :boards in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 HogeKeijiban::Application.routes.draw do
-  get "boards/index"
 
-  get "boards/new"
-
-  get "boards/create"
-
-  get "boards/show"
-
-  get "boards/edit"
-
-  get "boards/update"
-
-  get "boards/destroy"
+  resources :boards
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
